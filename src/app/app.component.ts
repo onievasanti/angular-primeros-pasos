bd85fc0d43b0ad21059d9eeba6acb32f65108564
Santi Onieva <s.onieva@sapalomera.cat>
2024-07-11 11:27:06 +0200

    Counter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+
+  public title: string = 'Hola Mundo';
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
+
 }
